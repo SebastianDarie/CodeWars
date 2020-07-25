fd568693002c149d60c00dcb59ce080a95e4b5c6
Sebastian Darie <s.d.01.2002@gmail.com>
2020-07-25 22:12:37 +0300

    Add files via upload

---
+++ pig latin.js
@@ -0,0 +1,9 @@
+function pigIt(str){
+    str = str.split(' ')
+    for(let i = 0; i<str.length; i++){
+        if(/[a-zA-Z]/.test(str[i])){
+            str[i] = str[i].slice(1) + str[i][0] + 'ay'
+        } 
+    }
+    return str.join(' ')
+}
